c82c27cdd0ae31a4a323481d26c09915cb2a0140
beardedeagle <randy@heroictek.com>
2018-07-30 01:41:57 -0500

    Adding more tests
    - Removed ex_unit_clustered_case from mix.lock
    - Added a couple of other tests for the mnesiac module

--- test/mnesiac_test.exs
+++ test/mnesiac_test.exs
@@ -17,4 +17,20 @@ defmodule MnesiacTest do
     assert Enum.member?(tables, ExampleStore) == true
     assert :mnesia.system_info(:schema_location) == :opt_disc
   end
+
+  test "cluster status" do
+    assert Mnesiac.cluster_status == [{:running_nodes, [Node.self()]}]
+  end
+
+  test "running nodes" do
+    assert Mnesiac.running_nodes == [Node.self()]
+  end
+
+  test "node in cluster" do
+    assert Mnesiac.node_in_cluster?(Node.self()) == true
+  end
+
+  test "running db node" do
+    assert Mnesiac.running_db_node?(Node.self()) == true
+  end
 end
