9777a82fcdeb91e3fd9ff64368f27655bd70a6c9
beardedeagle <randy@heroictek.com>
2018-07-30 00:21:06 -0500

    Removing clustered_case
    - Removing ex_unit_clustered_case until I can get direction on how to get it to properly work
    - Updated .gitignore
    - Added simple test for bringning mnesiac up in single node configuration

--- mix.exs
+++ mix.exs
@@ -60,7 +60,6 @@ defmodule Mnesiac.MixProject do
       {:credo, "~> 0.10", only: [:dev], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.19", only: [:dev], runtime: false},
-      {:ex_unit_clustered_case, github: "bitwalker/ex_unit_clustered_case", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.9", only: [:dev, :test], runtime: false}
     ]
   end
--- test/mnesiac_test.exs
+++ test/mnesiac_test.exs
@@ -3,7 +3,18 @@ defmodule MnesiacTest do
   use ExUnit.Case
   doctest Mnesiac
 
-  test "sample test" do
-    assert :ok
+  setup_all do
+    node = Node.self()
+    Mnesiac.init_mnesia([node])
+
+    :ok
+  end
+
+  test "tables exist" do
+    tables = :mnesia.system_info(:tables)
+
+    assert Enum.member?(tables, :schema) == true
+    assert Enum.member?(tables, ExampleStore) == true
+    assert :mnesia.system_info(:schema_location) == :opt_disc
   end
 end
