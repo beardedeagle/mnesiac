81429a36dea0e2f78ea0787b7b197c532d027ae1
beardedeagle <randy@heroictek.com>
2018-07-30 01:54:40 -0500

    Bumping version
    - Bumping mnesiac version to 0.2.0
    - Updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.2.0 (July 30, 2018)
+
+- Added additional tests
+- Added .github folder and files
+
 ## 0.1.0 (July 29, 2018)
 
 - Initial release
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Mnesiac.MixProject do
   def project do
     [
       app: :mnesiac,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
