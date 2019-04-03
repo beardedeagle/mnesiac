521334b9dcbd9dc73daf5b6aa5b882b77e814c0b
beardedeagle <randy@heroictek.com>
2019-04-03 10:53:59 -0500

    [WIP] rf: completing supervisor todo (broken)
    [skip travis]
    - STILL VERY VERY BROKEN. DO NOT USE
    - added typespecs to define expected config structure for default library usage
    - removed config from formatter

--- lib/mnesiac/supervisor.ex
+++ lib/mnesiac/supervisor.ex
@@ -1,18 +1,39 @@
 defmodule Mnesiac.Supervisor do
   @moduledoc """
   Mnesiac supervisor.
-
-  TODO: Docs, probably talk about configuration to be passed in or type it out.
   """
   use Supervisor
+
+  @typedoc """
+  Default arguments expected to be passed in to Mnesiac. `override` is optional.
+  """
+  @type arg :: {:cluster, [node()]} | {:config, config} | {:override, (... -> struct()) | nil}
+
+  @typedoc """
+  Default implementation of arguments expected to be passed in to Mnesiac.
+  """
+  @type args :: [arg]
+
+  @typedoc """
+  Default configuration expected to be passed in to Mnesiac. `store_load_timeout` is optional.
+  """
+  @type config :: {:schema, store} | {:stores, [store, ...]} | {:store_load_timeout, integer()}
+
+  @typedoc """
+  Default store configuration expected to be passed in to Mnesiac. Everything is optional except `ref`.
+  """
+  @type store ::
+          {:ref, module() | atom()}
+          | {:ram_copies, [non_neg_integer() | float() | node()]}
+          | {:disc_copies, [non_neg_integer() | float() | node()]}
+          | {:disc_only_copies, [non_neg_integer() | float() | node()]}
+          | {:blacklist, [node()]}
+          | {:migrations, [mfa()]}
 
   @doc """
   Entry point for Mnesiac when used in a supervision tree.
   """
-  @spec start_link(
-          args :: [[cluster: [node()], config: keyword()] | keyword()] | [cluster: [node()], config: keyword()]
-        ) ::
-          :ignore | {:error, term()} | {:ok, pid()}
+  @spec start_link(args :: [args | keyword()] | args) :: :ignore | {:error, term()} | {:ok, pid()}
   def start_link([[cluster: _cluster, config: _config], opts] = args) do
     Supervisor.start_link(__MODULE__, args, opts)
   end
